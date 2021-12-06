3a545d1c10d593dc2af0240907e3ef0436129c30
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2021-12-06 15:28:45 +0800

    Take action assets

--- public/javascripts/take-action.js
+++ public/javascripts/take-action.js
@@ -3,6 +3,7 @@ var canvas = new fabric.Canvas('customize-area');
 $(document).ready(function(){
     $(this).click( function (){
         var imageSource = $(this 'img').attr('src');
+        console.log(imageSource);
         addImageToCanvas(imageSource);
     });
 });
