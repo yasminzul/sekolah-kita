840d5b88f31d2d6cf64009c31533289d5ffa0df1
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2021-12-12 18:15:43 +0800

    adjusted timeline canvas for mobile

--- public/javascripts/orang-asli.js
+++ public/javascripts/orang-asli.js
@@ -1,19 +1,9 @@
 const pixiapp = new window.PIXI.Application({ transparent: true });
-//$('body').append(pixiapp.view);
 $('#timeline').append(pixiapp.view);
 
-pixiapp.renderer.resize(window.innerWidth/2, (window.innerHeight/100)*100);
-// pixiapp.renderer.view.style.position = 'absolute';
-// pixiapp.renderer.view.style.zIndex = '-10';
-
 //elements
-const elemTimeline = PIXI.Sprite.from('images/Chapter1Timeline.PNG');
-elemTimeline.scale.set(0.15);
+const elemTimeline = PIXI.Sprite.from('images/OATimeline.PNG');
 pixiapp.stage.addChild(elemTimeline);
-
-elemTimeline.anchor.set(0.5);
-elemTimeline.position.set(pixiapp.screen.width / 2, pixiapp.screen.height / 4)
-
 
 //text elements
 const textStyle = new PIXI.TextStyle({
@@ -22,7 +12,7 @@ const textStyle = new PIXI.TextStyle({
     fill: '#000',
     align: 'center',
     wordWrap : true,
-    wordWrapWidth: 350
+    wordWrapWidth: pixiapp.screen.width - 100
 });
 
 let textArr = [
@@ -44,48 +34,170 @@ textCounter = 0;
 
 let basicText = new PIXI.Text(textArr[0], textStyle);
 
-basicText.position.set(pixiapp.screen.width / 2 - 155, pixiapp.screen.height/ 2)
-
-// basicText.x = 50;
-// basicText.y = 500;
-
+basicText.anchor.set(0.5);
 pixiapp.stage.addChild(basicText);
 
+const ease = new Ease.Ease();
+let animationTimeline = [];
+
+
+var x = window.matchMedia("(orientation: portrait)")
+
+//mobile
+if (x.matches)
+{
+
+  pixiapp.renderer.resize(window.innerWidth, window.innerHeight/2);
+
+  elemTimeline.scale.set(0.07);
+  elemTimeline.anchor.set(0.5);
+  elemTimeline.position.set(pixiapp.screen.width / 2, pixiapp.screen.height / 4)
+
+  basicText.position.set(pixiapp.screen.width/ 2, pixiapp.screen.height/ 2 + 100);
+
+  //timeline animation
+  animationTimeline = [
+  function(){ ease.add(
+      elemTimeline,
+      { x: (pixiapp.screen.width*2) + 20, scale: 0.25},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: (pixiapp.screen.width*2) - 250, scale: 0.25},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: (pixiapp.screen.width*2) - 525, scale: 0.25},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: pixiapp.screen.width - 30, scale: 0.25},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: 275, scale: 0.35},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: 25, scale: 0.5},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: -220, scale: 0.5},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: -pixiapp.screen.width + 220, scale: 0.5},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: -pixiapp.screen.width + 50, scale: 0.5},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){ ease.add(
+      elemTimeline,
+      { x: -pixiapp.screen.width - 235, scale: 0.5},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  }
+  ];
+
+}
+//end mobile
+
+//desktop
+else {
+
+pixiapp.renderer.resize(window.innerWidth/2, (window.innerHeight/100)*100);
+
+elemTimeline.scale.set(0.1);
+elemTimeline.anchor.set(0.5);
+elemTimeline.position.set(pixiapp.screen.width / 2, pixiapp.screen.height / 4)
+
+basicText.position.set(pixiapp.screen.width/ 2, pixiapp.screen.height/ 2 + 100);
 
 //timeline animation
-const ease = new Ease.Ease();
-const animationTimeline = [
+animationTimeline = [
 function(){ ease.add(
     elemTimeline,
-    { x: pixiapp.screen.width + 280, scale: 0.32},
+    { x: (pixiapp.screen.width*2) + 20, scale: 0.35},
     { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
   );
 },
 function(){ ease.add(
     elemTimeline,
-    { x: pixiapp.screen.width, scale: 0.32},
+    { x: (pixiapp.screen.width*2) - 250, scale: 0.35},
     { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
   );
 },
 function(){ ease.add(
     elemTimeline,
-    { x: pixiapp.screen.width - 200, scale: 0.32},
+    { x: (pixiapp.screen.width*2) - 525, scale: 0.35},
     { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
   );
 },
 function(){ ease.add(
     elemTimeline,
-    { x: pixiapp.screen.width - 420, scale: 0.32},
+    { x: pixiapp.screen.width - 30, scale: 0.35},
     { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
   );
 },
 function(){ ease.add(
     elemTimeline,
-    { x: -120, scale: 0.5},
+    { x: 275, scale: 0.5},
+    { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+  );
+},
+function(){ ease.add(
+    elemTimeline,
+    { x: 25, scale: 0.5},
+    { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+  );
+},
+function(){ ease.add(
+    elemTimeline,
+    { x: -220, scale: 0.5},
+    { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+  );
+},
+function(){ ease.add(
+    elemTimeline,
+    { x: -pixiapp.screen.width + 220, scale: 0.5},
+    { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+  );
+},
+function(){ ease.add(
+    elemTimeline,
+    { x: -pixiapp.screen.width + 50, scale: 0.5},
+    { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+  );
+},
+function(){ ease.add(
+    elemTimeline,
+    { x: -pixiapp.screen.width - 235, scale: 0.5},
     { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
   );
 }
-]
+];
+
+}
 
 var clickstate = -1;
 
@@ -112,14 +224,14 @@ $(document).ready(function(){
         animationTimeline[clickstate]();
 
       }
-      else if (textCounter == 11)
-      {
-         document.getElementById("timeline-next").disabled = true;
-      }
       else if (clickstate >= animationTimeline.length-1 && clickstate < 11)
       {
         clickstate += 1;
+      }
 
+      if (textCounter == 11)
+      {
+         document.getElementById("timeline-next").disabled = true;
       }
     });
 
@@ -151,7 +263,7 @@ $(document).ready(function(){
       {
         ease.add(
             elemTimeline,
-            { x: pixiapp.screen.width/2, y:pixiapp.screen.height/4, scale: 0.15},
+            { x: pixiapp.screen.width/2, y:pixiapp.screen.height/4, scale: 0.1},
             { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
           );
           clickstate -= 1;
