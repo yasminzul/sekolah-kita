fa34f351694f1dbe65ff5b2ee21f333b9d917f67
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2021-12-22 21:48:22 +0800

    initial set up for Take Action

--- public/javascripts/take-action.js
+++ public/javascripts/take-action.js
@@ -1,16 +1,107 @@
-var canvas = new fabric.Canvas('customize-area');
+var x = window.matchMedia("(max-width: 500px)")
+
+if (x.matches){
+
+  var canvas = new fabric.Canvas('customize-area', {
+    width: 310,
+    height: 310
+  });
+
+  var textbox = new fabric.Textbox('Pendidikan tanpa batasan!\nKetahui bagaimana dengan layari sekolahkita.my', {
+    left: 65,
+    top: 28,
+    width: 200,
+    fontSize: 16,
+    fontFamily: 'Archer Book',
+    fontWeight: 'bold',
+    borderColor: '#D1C7BA',
+    cornerColor: '#D1C7BA',
+    cornerSize: 10,
+    transparentCorners: false
+  });
+  canvas.add(textbox).setActiveObject(textbox);
+
+}
+
+else{
+
+  var canvas = new fabric.Canvas('customize-area', {
+    width: 620,
+    height: 620
+  });
+
+  var textbox = new fabric.Textbox('Pendidikan tanpa batasan!\nKetahui bagaimana dengan layari sekolahkita.my', {
+    left: 145,
+    top: 60,
+    width: 320,
+    fontSize: 26,
+    fontFamily: 'Archer Book',
+    fontWeight: 'bold',
+    borderColor: '#D1C7BA',
+    cornerColor: '#D1C7BA',
+    cornerSize: 10,
+    transparentCorners: false
+  });
+  canvas.add(textbox).setActiveObject(textbox);
+
+}
+
+
+
+fabric.Image.fromURL('/images/Bamboo-classroom.PNG', function(img){
+ img.scaleToWidth(canvas.width);
+ img.scaleToHeight(canvas.height);
+ canvas.setBackgroundImage(img,canvas.renderAll.bind(canvas));
+});
 
 $(document).ready(function(){
-    $('img').click( function (){
-        var imageSource = $(this).attr('src');
-        console.log(imageSource);
+
+    $('.sticker-bg').click( function (){
+        var imageSource = $('img', $(this)).attr('src');
+        changeCanvasBG(imageSource);
+    });
+
+    $('.sticker').click( function (){
+        var imageSource = $('img', $(this)).attr('src');
         addImageToCanvas(imageSource);
     });
+
+    $('#delete-sticker').click( function (){
+        canvas.remove(canvas.getActiveObject());
+    });
+
+    $('#no-bg').click( function (){
+      const image = new fabric.Image('');
+      canvas.setBackgroundImage(image, canvas.renderAll.bind(canvas));
+    });
+
 });
+
 
 function addImageToCanvas(imageNameURL)
 {
     fabric.Image.fromURL(imageNameURL, function(oImg) {
+      // oImg.scaleToWidth(85);
+      oImg.scaleX = 0.3;
+      oImg.scaleY = 0.3;
+      oImg.set({
+        'top': canvas.height/2-40,
+        'left': canvas.width/2-40,
+        borderColor: '#D1C7BA',
+        cornerColor: '#D1C7BA',
+        cornerSize: 10,
+        transparentCorners: false
+    });
       canvas.add(oImg);
     });
+}
+
+function changeCanvasBG(imageURL)
+{
+  fabric.Image.fromURL(imageURL, function(img){
+   img.scaleToWidth(canvas.width);
+   img.scaleToHeight(canvas.height);
+   canvas.setBackgroundImage(img);
+   canvas.requestRenderAll();
+});
 }
