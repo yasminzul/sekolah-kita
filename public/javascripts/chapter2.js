92545206a6f35ea30d32a70f50cf66e861ce730b
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2022-01-12 21:56:19 +0800

    Update chapter2.js

--- public/javascripts/chapter2.js
+++ public/javascripts/chapter2.js
@@ -61,22 +61,263 @@ function setup(loader, resources)
 
 
 
+  let sekolahAnimation = [
+  function(){
+    pixiapp.stage.removeChild(elemSekolah0);
+    elemSekolah1.anchor.set(0.5);
+    elemSekolah1.position.set(pixiapp.screen.width / 2.05, pixiapp.screen.height / 1.8);
+    pixiapp.stage.addChild(elemSekolah1);
+    ease.add(
+      elemSekolah1,
+      { x: (pixiapp.screen.width/100)*70, y: (pixiapp.screen.height/100)*65, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah1);
+    elemSekolah2.anchor.set(0.5);
+    elemSekolah2.scale.set(1.2);
+    elemSekolah2.position.set((pixiapp.screen.width/100)*70, (pixiapp.screen.height/100)*65);
+    pixiapp.stage.addChild(elemSekolah2);
+    ease.add(
+      elemSekolah2,
+      { x: (pixiapp.screen.width/100)*60, y: (pixiapp.screen.height/100)*95, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah2);
+    elemSekolah3.anchor.set(0.5);
+    elemSekolah3.scale.set(1.2);
+    elemSekolah3.position.set((pixiapp.screen.width/100)*60, (pixiapp.screen.height/100)*95);
+    pixiapp.stage.addChild(elemSekolah3);
+    ease.add(
+      elemSekolah3,
+      { x: (pixiapp.screen.width/100)*75, y: (pixiapp.screen.height/100)*50, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah3);
+    elemSekolah4.anchor.set(0.5);
+    elemSekolah4.scale.set(1.2);
+    elemSekolah4.position.set((pixiapp.screen.width/100)*75, (pixiapp.screen.height/100)*50);
+    pixiapp.stage.addChild(elemSekolah4);
+    ease.add(
+      elemSekolah4,
+      { x: (pixiapp.screen.width/100)*60, y: (pixiapp.screen.height/100)*30, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah4);
+    elemSekolah5.anchor.set(0.5);
+    elemSekolah5.scale.set(1.2);
+    elemSekolah5.position.set((pixiapp.screen.width/100)*60,(pixiapp.screen.height/100)*30);
+    pixiapp.stage.addChild(elemSekolah5);
+    ease.add(
+      elemSekolah5,
+      { x: (pixiapp.screen.width/100)*60, y: (pixiapp.screen.height/100)*12, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah5);
+    elemSekolah6.anchor.set(0.5);
+    elemSekolah6.scale.set(1.2);
+    elemSekolah6.position.set((pixiapp.screen.width/100)*60,(pixiapp.screen.height/100)*12);
+    pixiapp.stage.addChild(elemSekolah6);
+    ease.add(
+      elemSekolah6,
+      { x: (pixiapp.screen.width/100)*25, y: (pixiapp.screen.height/100)*100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah6);
+    elemSekolah7.anchor.set(0.5);
+    elemSekolah7.scale.set(1.2);
+    elemSekolah7.position.set((pixiapp.screen.width/100)*25,(pixiapp.screen.height/100)*100);
+    pixiapp.stage.addChild(elemSekolah7);
+    ease.add(
+      elemSekolah7,
+      { x: (pixiapp.screen.width/100)*5, y: (pixiapp.screen.height/100)*80, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah7);
+    elemSekolah8.anchor.set(0.5);
+    elemSekolah8.scale.set(1.2);
+    elemSekolah8.position.set((pixiapp.screen.width/100)*5,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah8);
+    ease.add(
+      elemSekolah8,
+      { x: -(pixiapp.screen.width/100)*20, y: (pixiapp.screen.height/100)*80, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah9);
+    elemSekolah10.anchor.set(0.5);
+    elemSekolah10.scale.set(1.2);
+    elemSekolah10.position.set((pixiapp.screen.width/100)*2,pixiapp.screen.height/100);
+    pixiapp.stage.addChild(elemSekolah10);
+    ease.add(
+      elemSekolah10,
+      { x: (pixiapp.screen.width/100)*2, y: (pixiapp.screen.height/100)*30, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah10);
+    elemSekolah11.anchor.set(0.5);
+    elemSekolah11.scale.set(1.2);
+    elemSekolah11.position.set((pixiapp.screen.width/100)*2,(pixiapp.screen.height/100)*30);
+    pixiapp.stage.addChild(elemSekolah11);
+    ease.add(
+      elemSekolah11,
+      { x: -(pixiapp.screen.width/100)*20, y: (pixiapp.screen.height/100)*20, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah11);
+    elemSekolah12.anchor.set(0.5);
+    elemSekolah12.scale.set(1.2);
+    elemSekolah12.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*20);
+    pixiapp.stage.addChild(elemSekolah12);
+    ease.add(
+      elemSekolah12,
+      { x: -(pixiapp.screen.width/100)*15, y: (pixiapp.screen.height/100)*60, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah12);
+    elemSekolah13.anchor.set(0.5);
+    elemSekolah13.scale.set(1.2);
+    elemSekolah13.position.set(-(pixiapp.screen.width/100)*15,(pixiapp.screen.height/100)*60);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah13,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  },
+  function(){
+    pixiapp.stage.removeChild(elemSekolah8);
+    elemSekolah9.anchor.set(0.5);
+    elemSekolah9.scale.set(1.2);
+    elemSekolah9.position.set(-(pixiapp.screen.width/100)*20,(pixiapp.screen.height/100)*80);
+    pixiapp.stage.addChild(elemSekolah9);
+    ease.add(
+      elemSekolah9,
+      { x: (pixiapp.screen.width/100)*2, y: pixiapp.screen.height/100, scale: 1.2},
+      { reverse: false, duration: 1500, ease: 'easeInOutQuad' }
+    );
+  }
+  ];
 
 
-  var clickstate = 0;
+    var clickstate = 0;
 
-  $(document).ready(function(){
+    $(document).ready(function(){
 
-    $('#school-next').click( function (){
-      sekolahAnimation[clickstate]();
-      clickstate += 1;
+      $('#school-next').click( function (){
+        sekolahAnimation[clickstate]();
+        clickstate += 1;
+      });
+
+      $('#school-prev').click( function (){
+        sekolahAnimation[clickstate]();
+        clickstate -= 1;
+      });
+
     });
-
-    $('#school-prev').click( function (){
-      sekolahAnimation[clickstate]();
-      clickstate -= 1;
-    });
-
-  });
 
 }
