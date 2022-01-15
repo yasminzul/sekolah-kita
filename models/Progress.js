c945f8a5525cf69ac553ce1d3f03e0805e49310c
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2022-01-16 07:23:05 +0800

    first attempt

---
+++ models/Progress.js
@@ -0,0 +1,38 @@
+// const mongoose = require('mongoose');
+//
+// const progressSchema = new mongoose.Schema ({
+//   cookieid :
+//   {
+//     type: String
+//   },
+//   clue1 :
+//   {
+//     type: boolean
+//   },
+//   clue2 :
+//   {
+//     type: boolean
+//   },
+//   clue3 :
+//   {
+//     type: boolean
+//   },
+//   clue4 :
+//   {
+//     type: boolean
+//   },
+//   clue5 :
+//   {
+//     type: boolean
+//   },
+//   clue6 :
+//   {
+//     type: boolean
+//   },
+//   chapteraccess:
+//   {
+//     type: int
+//   }
+// });
+//
+// module.exports = mongoose.model('Progress', progressSchema );
