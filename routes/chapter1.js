44349666ccfb335cdcf2206771469433592162ab
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2022-01-20 22:54:49 +0800

    found clue highlighted

--- routes/chapter1.js
+++ routes/chapter1.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var Scorecard = require ('../models/treasure-hunt');
 
-/* GET orang asli page. */
-router.get('/', function(req, res, next) {
+/* GET chapter 1 page. */
+router.get('/', function(req, res) {
   var scoreCard = new Scorecard(req.session.scoreCard ? req.session.scoreCard : {
   clue: [false, false, false, false, false, false, false, false, false, false, false, false],
   chapterAccess:1,
   cluesFound:0 });
-  res.render('chapter1', { title: 'Chapter 1', currScore: scoreCard.cluesFound });
+  res.render('chapter1', { title: 'Chapter 1', currScore: scoreCard });
 });
 
 router.get('/found-clue/:id', function(req, res, next) {
